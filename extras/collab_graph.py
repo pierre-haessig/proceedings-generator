4d0bbd054cb23628fd24fbe1564b8d4c54313a92
Pierre Haessig <pierre.haessig@crans.org>
2014-06-24 18:04:50 +0200

    add collaboration graphs

---
+++ extras/collab_graph.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Pierre Haessig — June 2014
+""" Analyze collaboration between labs using conference data:
+each jointly written article creates a link.
+"""
+
+from __future__ import division, print_function, unicode_literals
+
+from os.path import join
+from itertools import combinations
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from data_reader import (
+    read_articles)
+
+### Read configuration
+config = {}
+execfile('config.py', {}, config)
+
+config_vars = config['c']
+data = config['data']
+
+### Read and process the list of articles
+articles = list(read_articles(join(data['path'], data['article_table'])) )
+header, articles = articles[0], articles[1:]
+
+# sort articles list by first author
+articles.sort(key= lambda item: item['authors'])
+
+print('{:d} articles read from table "{}"'.format(len(articles),
+                                                data['article_table']))
+
+def name_fix(lab):
+    lab = lab.replace(' (France)', '')
+    return lab
+    
+def name_format(lab):
+    split = lab.split(' ')
+    # Put some '\n'
+    stack = [split[0]]
+    for s in split[1:]:
+        if len(stack[-1] + s) < 15:
+            stack[-1] = stack[-1] + ' ' + s
+        else:
+            stack.append(s)
+    
+    lab = '\n'.join(stack)
+    return lab
+
+# Build the collaboration graph:
+collab = {}
+
+article_cnt = {}
+
+for art in articles:
+    labs = [name_fix(lab) for lab in art['labs_split']]
+    labs = set(labs)
+    # browse all pairs:
+    for l1, l2 in combinations(labs, 2):
+        # 1) mark l1 -> l2
+        if not l1 in collab:
+            collab[l1] = {}
+        if not l2 in collab[l1]:
+            collab[l1][l2] = 0
+        collab[l1][l2] += 1
+#        # 2) mark l2 -> l1
+#        if not l2 in collab:
+#            collab[l2] = {}
+#        if not l1 in collab[l2]:
+#            collab[l2][l1] = 0
+#        collab[l2][l1] += 1
+    # Count articles:
+    for lab in labs:
+        if lab not in article_cnt:
+            article_cnt[lab] = 0
+        article_cnt[lab] += 1
+
+def edge_gen(collab, thres=1):
+    for l1 in collab:
+        for l2 in collab[l1]:
+            cnt = collab[l1][l2]
+            if cnt >= thres:
+                yield (l1, l2, {'weight': 1./cnt})
+
+G = nx.Graph()
+
+G.add_edges_from(edge_gen(collab, 1))
+
+pos = nx.spring_layout(G, weight='weight')
+pos = nx.graphviz_layout(G, 'neato', args='-Goverlap=false')
+
+### Draw:
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111)
+
+# nodes
+node_size = [50*article_cnt[lab] for lab in G.nodes()]
+nx.draw_networkx_nodes(G, pos, node_size=node_size,
+                       node_color='#99caff', linewidths=0.1)
+
+fonts = dict(font_size=3,font_family='sans-serif')
+nx.draw_networkx_labels(G, pos, labels = {lab: name_format(lab) for lab in G.nodes()}, **fonts)
+
+
+edge_width = [3*e[2]['weight'] for e in G.edges(data=True)]
+nx.draw_networkx_edges(G, pos, width=edge_width,
+                       edge_color='#FFAA00', alpha=0.5)
+
+fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+#plt.xlim(-0.06, 1.06)
+#plt.ylim(-0.06, 1.06)
+plt.axis('off')
+plt.show()
+fig.savefig('collab_neato3.png', dpi=300, bbox_inches='tight')
+
+
